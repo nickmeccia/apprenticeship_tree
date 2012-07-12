fc3245f1b4f2b5fdc2d18e9a0dbc23603c5c885b
Nick Meccia <nickmeccia@gmail.com>
2012-07-12 10:57:06 -0500

    Added styles for iPhone and iPad.

---
+++ README.markdown
@@ -0,0 +1,6 @@
+8th Light Apprenticeship Tree
+-----------------------------
+
+This is a website which showcases the patriarchy of 8th Light mentors and apprentices as if it were a family tree.
+
+Credit goes to mg50 for the use of Carousel.js, found here: https://github.com/mg50/Carousel
--- src/apprenticeship_tree/view/layout.hiccup.clj
+++ src/apprenticeship_tree/view/layout.hiccup.clj
@@ -2,6 +2,7 @@
 [:html
  [:head
   [:meta {:http-equiv "Content-Type" :content "text/html" :charset "iso-8859-1"}]
+	[:meta {:name "viewport" :content "width=device-width"}]
   [:title "apprenticeship_tree"]
   (include-css "/stylesheets/reset.css")
   (include-css "/stylesheets/apprenticeship_tree.css")
